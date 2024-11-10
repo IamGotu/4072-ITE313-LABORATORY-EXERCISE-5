8ef145f8f2ac279998c48b5f7d35a3399fbe928f
Mark John <markjohnjopia1@gmail.com>
2024-11-11 01:09:19 +0800

    changin it to home logo

--- social_media_app/resources/views/layouts/navigation.blade.php
+++ social_media_app/resources/views/layouts/navigation.blade.php
@@ -13,7 +13,11 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        <!-- Home Icon SVG -->
+                        <svg class="w-6 h-6 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-4 0h4"></path>
+                        </svg>
+                        <span class="sr-only">{{ __('Home') }}</span>
                     </x-nav-link>
                 </div>
             </div>
